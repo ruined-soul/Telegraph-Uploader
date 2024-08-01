adc178442bbd7c5bbec5765645b47b6b9e98ecfd
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-08-01 10:40:22 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7304879730:AAHWnILVrNQjeD7QuLMd3UOuC5xf72mzd5I")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 2170492))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "82b683da442942d5c177ec520318a32f")
